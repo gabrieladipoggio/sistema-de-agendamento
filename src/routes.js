fd91848eb4306ee4a9a5306914deabbf876df6cd
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-08-22 15:36:20 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+// import database from './database'
+
+
+const routes = new Router();   // Endereços da interface 
+
+routes.post('/users', UserController.store)
+
+
+export default routes;
