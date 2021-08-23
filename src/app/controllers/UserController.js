92972eb0d3397ccd288c9374daba78f5129fd71a
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-08-22 22:43:20 -0300

    Adds password encryption

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,7 +2,20 @@ import User from '../models/User'
 
 class UserController {
     async store(req, res){
-        const user = await User.create(req.body)
+        const userExists = await User.findOne({
+            where:{
+                email: req.body.email
+            }
+        })
+
+        if (userExists){
+            return res.status(400).json({error: 'Usuário já cadastrado'})
+        }
+
+        const user = await User.create(req.body, {
+            returning: ["name", "email", "password_hash", "provider" ]
+        })
+        
         return res.json(user);
 
     }
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,18 +1,29 @@
-import Sequelize, {Model} from 'Sequelize';
+import Sequelize, { DataTypes, Model} from 'Sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
     static init(sequelize){
         super.init({
             name: Sequelize.STRING,
             email: Sequelize.STRING,
+            password: DataTypes.VIRTUAL,
             password_hash: Sequelize.STRING,
             provider: Sequelize.BOOLEAN,
-
         },
         {
             sequelize,
+
+        });
+
+        this.addHook('beforeSave', async (instance, options) => {
+            if (instance.password){
+                let result = await bcrypt.hash(instance.password, 10)
+                instance.password_hash = result
+            }
         })
-    }
+        return this;
+
+    } 
 }
 
 export default User;
