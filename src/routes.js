1c1719cb46fbe40f29fdaa66aee3863d5cfdec11
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-08-22 23:21:53 -0300

    Adds JWT authentication

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import database from './database'
-
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();   // Endereços da interface 
 
 routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
 
 
 export default routes;
